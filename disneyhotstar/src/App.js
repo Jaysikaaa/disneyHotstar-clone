3d6445a261c0ff369ad07f2f9a784b242c68c60c
jaysika <jaysikamalhotra445@gmail.com>
2023-12-13 18:29:05 +0530

    created login component

--- disneyhotstar/src/App.js
+++ disneyhotstar/src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router,  Route, Routes } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+      <Router>
+     <Routes>
+          <Route exact path="/" element= {<Login/>}/>
+          </Routes>
+      </Router>
     </div>
   );
 }
